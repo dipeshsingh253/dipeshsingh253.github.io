6b46db0832bd89e43040be1c4bf357c2def0de81
dipeshsingh253 <sinhdipesh@gmail.com>
2022-11-02 03:20:26 +0530

    projects tab completed for large screens

--- scripts/main.js
+++ scripts/main.js
@@ -339,6 +339,7 @@ function projectsClick() {
     if (contact_id.classList.contains("vis")) {
         contact_id.classList.toggle("vis");
     }
+    loadProject();
     print();
 }
 
@@ -667,7 +668,7 @@ function projectsClickMobile() {
     }
 
     hamburgerToggle();
-
+    loadProject();
     print();
 }
 
@@ -798,6 +799,51 @@ function displayPercentageCreativity() {
         }
     }, 33);
 
+}
+
+function loadProject() {
+
+    $(function () {
+        $('.carousel-item').eq(0).addClass('active');
+        var total = $('.carousel-item').length;
+        var current = 0;
+        $('#moveRight').on('click', function () {
+            var next = current;
+            current = current + 1;
+            setSlide(next, current);
+        });
+        $('#moveLeft').on('click', function () {
+            var prev = current;
+            current = current - 1;
+            setSlide(prev, current);
+        });
+        function setSlide(prev, next) {
+            var slide = current;
+            if (next > total - 1) {
+                slide = 0;
+                current = 0;
+            }
+            if (next < 0) {
+                slide = total - 1;
+                current = total - 1;
+            }
+            $('.carousel-item').eq(prev).removeClass('active');
+            $('.carousel-item').eq(slide).addClass('active');
+            setTimeout(function () {
+
+            }, 800);
+
+
+
+            console.log('current ' + current);
+            console.log('prev ' + prev);
+
+            if(current == prev){
+                alert("No More Projects Available ...");
+            }
+
+        }
+    });
 }
 
 function print() {
